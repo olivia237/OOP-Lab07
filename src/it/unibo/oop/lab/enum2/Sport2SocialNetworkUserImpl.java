1a4b953e73b6bd4fbc60ee5e0c26f1ebcb3d0946
OLIVIA <oliviarannick@gmail.com>
2020-11-16 22:02:23 +0100

    es7

--- src/it/unibo/oop/lab/enum2/Sport2SocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/enum2/Sport2SocialNetworkUserImpl.java
@@ -30,7 +30,8 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      * 
      * add a field to keep track of the set of sports followed/done by a user
      */
-
+     private final Set<Sport>sports;
+	
     /**
      * Builds a new {@link Sport2SocialNetworkUserImpl}.
      * 
@@ -75,7 +76,7 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      *            a sport followed/done by the user
      */
     public void addSport(final Sport sport) {
-
+    return this.sports.add(sport);
     }
 
     /**
@@ -87,7 +88,7 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      * @return true if user likes sport s
      */
     public boolean likesSport(final Sport s) {
-        return false;
+        return this.sports.contains(s);
     }
 
     /*
@@ -100,18 +101,25 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      * 
      * @return the set of individual sport this user practices/follows
      */
-    /*
-     * public Set<Sport> getIndividualSports() { return null; }
-     * 
-     * 
-     * /** Returns the set of sports which are practiced in a given place.
+    
+     public Set<Sport> getIndividualSports() { 
+    	 if(this.sports.isIndividualSport() == true)
+    	 return this.sports;
+    	 }
+ 
+    
+     /** Returns the set of sports which are practiced in a given place.
      * 
      * @param p the place in which the sport is practiced in order to be
      * included in the resulting set
      * 
      * @return the set of sport practiced in a given place
      */
-    /*
-     * public Set<Sport> getSportPracticedInPlace(Place p) { return null; }
-     */
+   
+     public Set<Sport> getSportPracticedInPlace(Place p) {
+     if(this.sports.getplace() == "INDOOR") {
+    	 return sports;
+    
+     }
+}
 }
