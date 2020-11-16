300ceb6e425214b44bb4f5a2d9ebe812dc5a4f00
OLIVIA <oliviarannick@gmail.com>
2020-11-16 22:09:46 +0100

    es10

---
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -0,0 +1,14 @@
+package it.unibo.oop.lab.nesting2;
+/*import java.util.List;
+import java.util.Iterator;
+*/
+public class OneListAcceptable<T> implements Acceptable<T> {
+  private final List<T> list;
+
+	public Acceptor<T> acceptor() {
+
+		return null;
+	}
+	
+
+}
